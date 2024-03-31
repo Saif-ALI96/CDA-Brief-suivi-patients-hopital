98e4de4cb55cfd33eca340f116e96ba5886e50c5
Saif Ali <kattan.saifali@gmail.com>
2024-03-31 04:54:27 +0200

    add: the suivi patient appli

---
+++ suivi-patient /src/main/java/co/suivipatient/SuiviPatientApplication.java
@@ -0,0 +1,13 @@
+package co.suivipatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiviPatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuiviPatientApplication.class, args);
+	}
+
+}
