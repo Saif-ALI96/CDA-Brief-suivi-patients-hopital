f6e2ed9fe8335e7fd8235ddebef12171181900cf
Saif Ali <kattan.saifali@gmail.com>
2024-04-02 12:05:29 +0200

    add: le javadoc

--- suivi-patient /src/main/java/co/suivipatient/business/service/chambre/ChambreServiceImpl.java
+++ suivi-patient /src/main/java/co/suivipatient/business/service/chambre/ChambreServiceImpl.java
@@ -16,24 +16,42 @@ import co.suivipatient.persistance.repository.ChambreRepo;
  * Offre des fonctionnalités pour créer, lister, récupérer, modifier et supprimer des chambres,
  * en s'appuyant sur le repository ChambreRepo pour interagir avec la base de données.
  */
-@Service
-public class ChambreServiceImpl  implements ChambreService {
 
-  private ChambreRepo  chambreRepository;
+@Service
+public class ChambreServiceImpl implements ChambreService {
+
+  // Repository pour l'accès aux données des chambres
+  private ChambreRepo chambreRepository;
   
+  /**
+   * Constructeur de la classe ChambreServiceImpl.
+   * 
+   * @param chambreRepository Le repository pour l'accès aux données des chambres.
+   */
   public ChambreServiceImpl(ChambreRepo chambreRepository) {
     this.chambreRepository = chambreRepository;
   }
 
+  /**
+   * Crée une nouvelle chambre avec les informations spécifiées.
+   * 
+   * @param chambre Les informations de la chambre à créer.
+   * @return Le DTO de la chambre créée.
+   */
   @Override
   public ChambreDTO creer(ChambreDTO chambre) {
-    Chambre  chambreEntity = ChambreConvert.getInstance().convertChambreDtoToEntity(chambre);
+    Chambre chambreEntity = ChambreConvert.getInstance().convertChambreDtoToEntity(chambre);
 
     Chambre conserveEntity = chambreRepository.save(chambreEntity);
 
-    return  ChambreConvert.getInstance().convertEntityToChambreDto(conserveEntity);
+    return ChambreConvert.getInstance().convertEntityToChambreDto(conserveEntity);
   }
 
+  /**
+   * Récupère la liste de toutes les chambres disponibles.
+   * 
+   * @return Une liste de ChambreDTO contenant les informations sur les chambres.
+   */
   @Override
   public List<ChambreDTO> lister() {
     List<Chambre> listeEntitees = chambreRepository.findAll();
@@ -42,6 +60,12 @@ public class ChambreServiceImpl  implements ChambreService {
   }
   
 
+  /**
+   * Récupère les informations d'une chambre avec l'identifiant spécifié.
+   * 
+   * @param id L'identifiant de la chambre à récupérer.
+   * @return Un ChambreDTO contenant les informations sur la chambre.
+   */
   @Override
   public ChambreDTO liChambreDTO(long id) {
     Optional<Chambre> optionalChambre  = chambreRepository.findById(id);
@@ -51,34 +75,44 @@ public class ChambreServiceImpl  implements ChambreService {
     return ChambreConvert.getInstance().convertEntityToChambreDto(chambre);
   }
 
+  /**
+   * Modifie les informations d'une chambre avec l'identifiant spécifié.
+   * 
+   * @param chambre Les nouvelles informations de la chambre.
+   * @param id L'identifiant de la chambre à modifier.
+   * @return Le DTO de la chambre modifiée.
+   * @throws ServiceException Si la chambre avec l'identifiant spécifié n'existe pas.
+   */
   @Override
   public ChambreDTO modifiChambreDTO(ChambreDTO chambre, long id) {
     Optional<Chambre> chambreOptionel = chambreRepository.findById(id);
 
     if(chambreOptionel.isPresent()){
-      Chambre  chambreEnBaseDeDonnes= chambreOptionel.get();
+      Chambre chambreEnBaseDeDonnes = chambreOptionel.get();
       chambreEnBaseDeDonnes.setNumDeChambre(chambre.getNumDeChambre());
       chambreEnBaseDeDonnes.setNombreLit(chambre.getNombreLit());
 
-      Chambre  chambreModifiee = chambreRepository.save(chambreEnBaseDeDonnes);
+      Chambre chambreModifiee = chambreRepository.save(chambreEnBaseDeDonnes);
       return ChambreConvert.getInstance().convertEntityToChambreDto(chambreModifiee);
-    }else{
+    } else {
       throw new ServiceException("La chambre que vous voulez modifier n'existe pas");
     }
-
   }
 
+  /**
+   * Supprime une chambre avec l'identifiant spécifié.
+   * 
+   * @param id L'identifiant de la chambre à supprimer.
+   * @throws ServiceException Si la chambre avec l'identifiant spécifié n'existe pas.
+   */
   @Override
   public void supprimer(long id) {
-
     Optional<Chambre> chambreOptional = chambreRepository.findById(id);
 
-    if(chambreOptional.isPresent()){
+    if(chambreOptional.isPresent()) {
       chambreRepository.deleteById(id);
-    } else{
+    } else {
       throw new ServiceException("La chambre que vous voulez supprimer n'existe pas");
-    
     }
   }
-  
 }
